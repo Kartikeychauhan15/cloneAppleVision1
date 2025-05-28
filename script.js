07e73af1931b7c01e9ad14bada25e357586261dd
Kartikey chauhan <itskartikeyy15@gmail.com>
2025-05-28 17:01:33 +0530

    done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function loco(){
     gsap.registerPlugin(ScrollTrigger);
 
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
+// hi
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
